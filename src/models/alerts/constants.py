9ec7e6a70b4d41be20c053c304c84b9c4b94b89e
ennvault <ennvaultresources@gmail.com>
2017-12-19 19:57:38 -0600

    Adding Heroku files and reading enviroment files

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,8 @@
 __author__ = 'jslvtr'
 
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+URL = "https://api.mailgun.net/v3/sandbox2f4dbf0e1eb741be9ad55b07e48fae13.mailgun.org"
+API_KEY = "key-2b10baf4498cdfda02a2ebbc1a81a9d7"
+FROM = "Notifications <postmaster@sandbox2f4dbf0e1eb741be9ad55b07e48fae13.mailgun.org>"
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
